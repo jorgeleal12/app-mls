4a9a20438d7737665e5327f3e7bc1b6c53c3a30d
jorgeleal12 <sistemas@grupoempresarialcyc.com>
2020-02-26 19:38:06 -0500

    update

--- src/app/utilitis/constant.ts
+++ src/app/utilitis/constant.ts
@@ -1,6 +1,6 @@
 export class constant {
-    public routeGlobal: String = "http://192.168.1.31/laravel-mls/public/api/"; ///sip/public/api/
-    public routeImage: String = "http://192.168.1.31/laravel-mls/public/public/"; ///sip/public/api/
+    public routeGlobal: String = "http://190.145.99.254:81/laravel-mls/public/api/"; ///sip/public/api/
+    public routeImage: String = "http://190.145.99.254:81/laravel-mls/public/public/"; ///sip/public/api/
 
 
 }
